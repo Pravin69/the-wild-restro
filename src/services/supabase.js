b7ecf35f42490a18854cf9adc2420d58fb3b0e5c
Pravin69 <p.thakur.133.ram@gmail.com>
2024-01-14 10:07:01 +0530

    Added .env and demo folder

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://cgwuipbzalztlntafqxf.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnd3VpcGJ6YWx6dGxudGFmcXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI0NTc0MTEsImV4cCI6MjAwODAzMzQxMX0.PcUmCwDnmIqyiWUhw5U7f3hzS2XDSQ0GJpiegLyizvc";
+const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
